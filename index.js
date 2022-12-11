1a96f40984e037c70be94c7ed2cf886c0ec36fe9
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2022-12-11 18:21:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+/*var numberofbuttons = document.querySelectorAll(".drum").length;
+
+//button press
+for (var i=0;i<numberofbuttons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML);
+        buttonanimation(buttonInnerHTML);
+    });
+}
+
+//keyboard press
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonanimation(event.key);
+})
+
+function makesound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;                
+        case "j":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        default:console.log(buttonInnerHTML);
+    }
+}
+
+function buttonanimation(currentkey){
+    var activebutton = document.querySelector("."+currentkey);
+    // document.querySelector(".set").classList.add("pressed");
+    activebutton.classList.add("pressed");
+    setTimeout(function() {
+        activebutton.classList.remove("pressed");
+    }, 100);
+}*/
+
+
+var numberofbuttons = document.querySelectorAll(".drum").length;
+
+//button press
+for (var i=0;i<numberofbuttons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML);
+        buttonanimation(buttonInnerHTML);
+    });
+}
+
+//keyboard press
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonanimation(event.key);
+})
+
+function makesound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio('tom-1.mp3');
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio('tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('tom-4.mp3');
+            audio.play();
+            break;                
+        case "j":
+            var audio = new Audio('kick-bass.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('snare.mp3');
+            audio.play();
+            break;
+        default:console.log(buttonInnerHTML);
+    }
+}
+
+function buttonanimation(currentkey){
+    var activebutton = document.querySelector("."+currentkey);
+    // document.querySelector(".set").classList.add("pressed");
+    activebutton.classList.add("pressed");
+    setTimeout(function() {
+        activebutton.classList.remove("pressed");
+    }, 100);
+}
